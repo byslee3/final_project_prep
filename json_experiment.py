7507fcb622cc2941c79c03c103b01fb1893bcfef
Christian <c@hackbright.com>
2012-11-08 12:59:17 -0800

    Day 4a: pulling all fans

--- json_experiment.py
+++ json_experiment.py
@@ -1,5 +1,6 @@
 import json
 import urllib
+import math
 
 
 ########## Currently pulling the JSON manually and storing it in text files
@@ -27,6 +28,12 @@ Page (from the shopping section) with a lot of items on it (try to cross referen
 http://www.polyvore.com/cgi/shop?brand=Giambattista+Valli&category_id=41&query=&.out=json
 json_shopping_page.txt
 ^^^^^ This one is not useful for our purposes. We can get the category tags from item page instead.
+
+Page (linked to the specific set) that holds all of the fans
+URL is determined by print_data_set function for the sample set that we picked
+json_set_all_fans_1.txt
+json_set_all_fans_2.txt
+json_set_all_fans_3.txt
 
 """
 
@@ -94,23 +101,75 @@ def print_data_set():
 
     d['test'] = "test value"
 
-    # Basic stuff
-    d ['set_id'] = polyvore["collection"]["id"]
-        # See what else you can pull from the "collection" field
+    # Basic stuff about the set
+    d['set_id'] = polyvore["collection"]["id"]
+    d['anchor'] = polyvore["collection"]["embed_anchor"]["anchor"]
+    d['pageviews'] = polyvore["collection"]["pageview"]
+    d['title'] = polyvore["collection"]["title"]
+    d['createdon'] = polyvore["collection"]["createdon"]
+    d['score'] = polyvore["collection"]["score"]
+    d['confirmed_tags'] = polyvore["confirmed_tags"]
 
     # Grabbing all the items in the set
-    d['num_set_items'] = len(polyvore["overlay_items"])
+    d['num_set_items_all'] = len(polyvore["overlay_items"])
+
     d['set_items'] = []
-    for i in range(d['num_set_items']):
-        item_id = polyvore["overlay_items"][i]['thing_id']
-        d['set_items'].append(item_id)
-        # Should this be pulled from the "collection" field instead?
+    d['set_items_names'] = []
+
+    for i in range(d['num_set_items_all']):
+
+        if polyvore["overlay_items"][i].get("is_product",0) == 1:
+
+            item_id = polyvore["overlay_items"][i]["thing_id"]
+            item_name = polyvore["overlay_items"][i]["title"]
+
+            d['set_items'].append(item_id)
+            d['set_items_names'].append(item_name)
+
+    d['num_set_items_valid'] = len(d['set_items'])
     
+    # Info about the creator
+    d['creator_id'] = polyvore["collection"]["user_id"]
+    d['creator_name'] = polyvore["collection"]["user_name"]
 
     # Need to pull the fan ids
     # But first need to get all the fan ids onto one page
     d['num_fans'] = polyvore["fav_count"]
+    d['guess_fan_json'] = []
 
+    fans = int(polyvore["fav_count"])
+    fans = 563  # ------------------ hard code this for now for this example
+    guess_length = 200.0
+    guess_pages = int(math.ceil(fans / guess_length))
+
+    # ------- Guess the URLs that the fans are stored on
+    for i in range(guess_pages):
+
+        url_1 = "http://www.polyvore.com/cgi/set.fans?.in=json&.out=jsonx&request=%7B\"id\"%3A\""
+        url_id = str(d['set_id'])
+        url_2 = "\"%2C\"length\"%3A"
+        url_length = str(int(guess_length))
+        url_3 = "%2C\"page\"%3A"
+        url_pages = str(i + 1)  # Because Polyvore page count starts at 1
+        url_4 = "%7D"
+
+        guess_url = url_1 + url_id + url_2 + url_length + url_3 + url_pages + url_4
+        
+        d['guess_fan_json'].append(guess_url)
+
+    # ------- Store the fan results into text files
+    # ------- Need to write this part
+
+    # ------- Loop through text files and pull fan ids from the text files
+
+    d['fan_ids'] = []
+
+    for i in range(guess_pages):
+        filename = "json_set_all_fans_" + str(i + 1) + ".txt" # -------- Will need to modify this later to include set id in file name
+        additional_fans = get_all_fans(filename)
+        d['fan_ids'].extend(additional_fans)
+
+    d['num_fan_ids'] = len(d['fan_ids']) # ------- This is just a check for now
 
     # Print out all the data at the end
     for key, value in d.iteritems():
@@ -119,6 +178,22 @@ def print_data_set():
         print value
 
 
+def get_all_fans(filename):
+
+    # Assume that we've used a separate function to pull all the JSON strings
+    # and store them in text files
+    
+    polyvore = get_JSON_dict(filename)
+
+    list_of_fans = polyvore["result"]["items"]
+    fan_ids = []
+    fan_names = []
+
+    for f in list_of_fans:
+        fan_ids.append(f["object_id"])
+        fan_names.append(f["user_name"])
+
+    return fan_ids
 
 
 
